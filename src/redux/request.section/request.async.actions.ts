02c81d7a17be67b58eb4eb70050a1d7e92cc16a1
Hrishikesh <hrishi.pethe@gmail.com>
2023-10-24 16:25:23 +0530

    update thunk action that triggers actual request to populate response store

--- src/redux/request.section/request.async.actions.ts
+++ src/redux/request.section/request.async.actions.ts
@@ -1,9 +1,10 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 import { v4 as uuidv4 } from "uuid";
-import { RequestModel } from "../../types";
-import { addToHistory } from "../request.history/history.reducer";
-
+import { ContentType, RequestModel } from "../../types";
+import { addtoHistoryAsync } from "../request.history/history.async.actions";
+import { determineBodytype, formatCode } from "../../lib/utils";
+import { setResponseMetadata } from "../response.section/response.reducer";
 
 export const makeRequestActionAsync = createAsyncThunk<void, void>('request-section/makeRequestActionAsync', async (_, thunkAPI) => {
 
@@ -11,19 +12,59 @@ export const makeRequestActionAsync = createAsyncThunk<void, void>('request-sect
 
     const rootState = getState() as RootState;
 
-    const requestInfo = rootState.requestStore;
+    const { url, method, query, headers } = rootState.requestStore;
+
 
     const newReqHistoryItem: RequestModel = {
         id: uuidv4(),
-        method: requestInfo.method,
-        name: requestInfo.url,
-        url: requestInfo.url,
-        query: requestInfo.query,
-        headers: requestInfo.headers,
+        method,
+        name: url,
+        url: url,
+        query: query,
+        headers: headers,
         triggered: new Date().getTime()
     }
 
-    dispatch(addToHistory(newReqHistoryItem));
+    const fetchHeaders: Record<string, any> = {};
+    for (const h of headers) {
+        if (h.enabled) {
+            if (h.name && h.value) {
+                fetchHeaders[h.name] = h.value;
+            }
+        }
+    }
 
+    const response = await fetch(url, {
+        method,
+        headers: fetchHeaders,
+    });
+
+    if (response.ok) {
+        const contentTypeHeader = response.headers.get("content-type");
+        let body: any;
+        let bodytype: ContentType = "unknown";
+        if (contentTypeHeader) {
+            bodytype = determineBodytype(contentTypeHeader);
+
+            if (["js", "json", "text", "html", "xml",].includes(bodytype)) {
+                const unformatted = await response.text();
+                body = await formatCode(unformatted, bodytype)
+            }
+            else {
+                body = await response.blob();
+            }
+        }
+        dispatch(setResponseMetadata({
+            body,
+            contentType: bodytype,
+            status: response.status,
+            statusText: response.statusText,
+            size: 0,
+            time: 0
+        }));
+
+    }
+
+    dispatch(addtoHistoryAsync(newReqHistoryItem));
 
 });
