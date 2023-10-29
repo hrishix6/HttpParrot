ef6aaa9d613ba6f4f3734c8382960363bab03f2b
Hrishikesh <hrishi.pethe@gmail.com>
2023-10-29 13:26:12 +0530

    add declaration for global variable  Handlebars added by hbs runtime script, add scripts to index.html

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,3 +6,7 @@ declare function js_beautify(str: string, options: any): string;
 declare function css_beautify(str: string, options: any): string;
 
 declare function html_beautify(str: string, options: any): string;
+
+declare namespace Handlebars {
+    export const templates: Record<string, any>
+}
