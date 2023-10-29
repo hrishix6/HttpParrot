33fcc7a2f31a3a9279a1c643a2faf705d2ac1b83
Hrishikesh <hrishi.pethe@gmail.com>
2023-10-29 13:26:58 +0530

    cosmetic changes to response section

--- src/components/domain/response.section/empty.body.tsx
+++ src/components/domain/response.section/empty.body.tsx
@@ -1,7 +1,7 @@
 export function EmptyBody() {
   return (
     <div className="absolute top-0 left-0 h-full w-full flex items-center justify-center">
-      <p className="text-lg font-semibold p-1 bg-background">
+      <p className="text-lg text-muted-foreground font-semibold p-1 bg-background">
         Response body will be visible here
       </p>
     </div>
