d96c10b4344bfe421c5543e015813c113542d3ac
Hrishikesh <hrishi.pethe@gmail.com>
2023-11-07 18:27:31 +0530

    update reducer of saving request data to index db to read from tabs reducer

--- src/components/domain/request.saved/request.saved.item.tsx
+++ src/components/domain/request.saved/request.saved.item.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 import { RequestModel } from '@/common/types';
 import { useAppDispatch } from '@/common/hoooks';
 import { deleteSavedRequestByIdAsync } from './redux/request.saved.async.actions';
-import { populateRequestSection } from '../request.section/redux/request.section.reducer';
+import { populateRequestSectionAsync } from '../tabs/redux/tabs.async.actions';
 
 interface RequestSavedItemProps {
   children?: React.ReactNode;
@@ -30,8 +30,11 @@ export function RequestSavedItem({
   };
 
   const handleViewInRequestSection = (r: RequestModel) => {
+    console.log(
+      `populating tab with request model ${JSON.stringify(r, null, 2)}`
+    );
     dispatch(
-      populateRequestSection({ model: r, mode: 'update', collectionName })
+      populateRequestSectionAsync({ model: r, mode: 'update', collectionName })
     );
   };
 
