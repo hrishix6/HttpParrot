8b924df77dd689260f8924f14315c4267c031e61
Hrishikesh <hrishi.pethe@gmail.com>
2023-11-18 17:09:04 +0530

    add some metadata to package json, increase chunksize warning limit to 1mb

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,14 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       app: "HttpParrot.html"
-  //     }
-  //   }
-  // },
+  build: {
+    rollupOptions: {
+      input: {
+        app: "HttpParrot.html"
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
